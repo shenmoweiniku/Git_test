2f4791da4a59ad39448f788195f7e44ea6999b38
123 <123.com>
2019-09-25 14:57:42 +0800

    Maven程序示例

---
+++ MyMaven/src/main/java/com/hunau/test/HelloController.java
@@ -0,0 +1,16 @@
+package com.hunau.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sang on 2018/7/4.
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "杜明灿,学号:201841882226";
+    }
+
+}
